78511fa905355a4c66c79622433f947b1a1e3496
Cristhy03 <158518090+Cristhy03@users.noreply.github.com>
2024-02-01 17:12:26 -0500

    first commit

---
+++ R/ejem1.R
@@ -0,0 +1,15 @@
+##########################################################################
+#########            Clase #43      #####################################
+#############      crear packages        ###############################
+######       Realizado por: Cristhiany Apunte    ######################
+######################################################################
+
+inversa_matriz <- function(matriz) {
+  if (det(matriz) == 0) {
+    stop("La matriz no tiene inversa, ya que su determinante es cero.")
+  }
+
+  inversa <- solve(matriz)
+  return(inversa)
+}
+
